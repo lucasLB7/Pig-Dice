45d867d50b7a45680047335af100e0a560f50812
Luka <plucaslambert@gmail.com>
2018-04-08 23:05:05 +0300

    Still plowing along with the jS.. bleh.. :p

--- jS/scripts.js
+++ jS/scripts.js
@@ -6,18 +6,29 @@ var player = function(name) {
   this.cacheScore = 0;
   this.totalScore = 0;
 }
+
+
 player.prototype.roll = function() {
   var rollDice = Math.floor(Math.random()*6+1);
+
+// WE CREATE A LOOP TO DISTINGUISH IF THE VALUE OF ROLLDICE IS 1 OR NOT..
+// AND WHAT HAPPENS ACCORDINGLY
   if (rollDice === 1) {
     this.cacheScore = 0;
+    return this.cacheScore;
   }
   else {
     this.cacheScore += rollDice;
+    console.log(this.cacheScore);
+    console.log(rollDice);
   }
-  return rollDice;
+  return this.cacheScore;
 }
+
+
 player.prototype.hold = function() {
-  this.cacheScore += this.totalScore;
+
+  this.totalScore += this.cacheScore;
   this.cacheScore = 0;
   return totalScore;
 }
@@ -35,8 +46,11 @@ $(document).ready(function() {
   $("form#playerNameInput").submit(function(event) {
     $(".gamescreen").show();
     $(".formgame").hide();
+// WE HAVE STATED THAT WE WANT TO HIDE THE FORM (formgame)
 
     event.preventDefault();
+
+
     var playerOneName = $("input#p1Name").val();
     var playerTwoName = $("input#p2Name").val();
     var maxScore = parseInt($("#maxScore").val());
@@ -44,11 +58,21 @@ $(document).ready(function() {
     var user1 = new player(playerOneName);
     var user2 = new player(playerTwoName);
 
+
+
     $("#nameStyle1").text(playerOneName);
-        $("#nameStyle2").text(playerTwoName);
+    $("#nameStyle2").text(playerTwoName);
+// WE PRINT THE VALUES OF PLAYER ONE & TWO ON THE WEBPAGE
+
+
 
     $("button#rolledDice1").click(function() {
       var rolledDice1 = user1.roll();
+
+      $("#player1Rolled").text("You have rolled: "+rolledDice1+"!");
+        var total1 = user1.totalScore;
+      $("#player1Score").text("Your total score is: "+total1+"!");
+
       if (rolledDice1 === 1) {
         $("#userOne").addClass("panel-disable");
         $("button#rolledDice1").addClass("button-disable");
@@ -58,10 +82,6 @@ $(document).ready(function() {
         $("button#rolledDice2").removeClass("button-disable");
         $("#holdDice2").removeClass("button-disable")
 
-        $("#player1Rolled").text("You have rolled: "+rolledDice1+"!");
-          var total1 = user1.totalScore;
-        $("#player1Score").text("You have rolled: "+total1+"!");
-
       }
       else {
         var cacheScore1 = user1.cacheScore;
@@ -69,10 +89,14 @@ $(document).ready(function() {
       }
     });
 
+
+
+
+    $("button#holdDice1").click(function() {
+      var holdDice1 = user1.hold();
+
+
+    });
+
   });
 });
-
-function turnDice() {
-  var img = document.getElementById("image");
-  img.setAttribute("class", "rotated-image");
-}
