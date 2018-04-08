345a339b2a250813cfd125cdfb791c9eca29b0f0
Luka <plucaslambert@gmail.com>
2018-04-08 10:12:30 +0300

    commit new logic

--- jS/scripts.js
+++ jS/scripts.js
@@ -1,67 +1,66 @@
- ..//let's start with the back end part of this story
-
-
-// we Declare an object oinkOink that we will keep using in the rest of the code...
- var oinkOink = {
- player1Score : 0,
- player2Score : 0,
-// These variables will hold the scores for the two players.
-// Scores will be added from the clearScore var (declared below)
-// This will only happen if the dice roll result is NOT EQUAL to one...
- clearScore : 0,
- currentPlayer : 1,
-
- function (diceRoll) {
-   var randomNum = math.floor(math.random()*6)+1; //We create a random number
-   console.log(randomNum); //Log it for debugging
-
-   if (randomNum === 1) { // If random number is equal to one:...
-     this.clearScore = 0; // The score goes to zero
-     this.changeTurns(); // We change turns (changeTurns) is defined below...
-   }
-   else {
-     this.clearScore += randomNum; // Else, if the randomNum is NOT 1,
-     //we add the value of randomNum to score.
-   }
-
-  function (changeTurns) {
-    if (currentPlayer === 1) { // If player one is currently active...
-      this.player1Score += score; // We add the score to player1Score
-      this.currentPlayer = 2; // We change player to 2
-    }
-    else { // if player two is active:
-      this.player2score += score; // We assign the value of score to player2score.
-      this.currentPlayer = 1; // We switch back to player 1
-    }
+var player = function(name) {
+  this.name = name;
+  this.cacheScore = 0;
+  this.totalScore = 0;
+}
+player.prototype.roll = function() {
+  var rollDice = Math.floor(Math.random()*6+1);
+  if (rollDice === 1) {
+    this.cacheScore = 0;
   }
-
-  function (holdBtn) { // Let's define the hold button what will simply return the function changeTurns
-    this.changeTurns();
-    this.clearScore = 0;
+  else {
+    this.cacheScore += rollDice;
   }
- }
-///////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
- // Back end DONE DEAL!! ;) Let's move on to the front end part of this....
+  return rollDice;
+}
+player.prototype.hold = function() {
+  this.totalScore += this.cacheScore;
+  this.cacheScore = 0;
+  return cacheScore;
+}
 
 
- var playerRoll = function() {
-     var dice = game.diceRoll();
-      $("#dice").text(dice);
-      changeTurns();
-   }
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
 
-   $("button#roll").click(function(){
-       playerRoll();
-     });
 
-     $("button#hold").click(function(){
-       game.hold();
-       changePlayerAndRefreshScores();
-     });
+$(document).ready(function() {
+  $("form#playerNameInput").submit(function(event) {
+    $(".gamescreen").show();
+    $(".formgame").hide();
+
+    event.preventDefault();
+    var playerOneName = $("input#p1Name").val();
+    var playerTwoName = $("input#p2Name").val();
+    var maxScore = parseInt($("#maxScore").val());
+
+    var user1 = new player(playerOneName);
+    var user2 = new player(playerTwoName);
+
+    $("button#rolledDice1").click(function() {
+      var rolledDice1 = user1.roll();
+      if (rolledDice1 === 1) {
+        $("#userOne").addClass("panel-disable");
+        $("button#rolledDice1").addClass("button-disable");
+        $("#holdDice1").addClass("button-disable")
+
+        $("#userTwo").removeClass("panel-disable");
+        $("button#rolledDice2").removeClass("button-disable");
+        $("#holdDice2").removeClass("button-disable")
+
+        $("#player1Rolled").text("You have rolled: "+rolledDice1+"!");
+          var total1 = user1.totalScore;
+        $("#player1Score").text("You have rolled: "+total1+"!");
+
+      }
+      else {
+        var cacheScore1 = user1.cacheScore;
+        $("#player1session").text("Session scrore is "+cacheScore1+"!");
+      }
+    });
+
+  });
+});
